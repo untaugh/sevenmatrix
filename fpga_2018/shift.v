5e346f74a1506f82fdbc5f5ef93e79e8bf2a27ae
Oskar Rundgren <orundg@gmail.com>
2018-02-09 19:40:22 +0000

    Clock is working. Only very small glitches.

--- fpga_2018/shift.v
+++ fpga_2018/shift.v
@@ -5,6 +5,7 @@
 `define DATA_SIZE 8192
 //`define DATA_SIZE 32
 `define CMD_READ 8'h03
+`define NUM_FRAMES 3
 
 module spi_shift(
 	input clk,
@@ -29,39 +30,49 @@ module spi_shift(
 
 wire accumulate;
 reg [30:0] Q;
-reg [31:0] cmd;
+wire [31:0] cmd;
 wire trig;
+wire [3:0] frame;
 
 reg [1:0] framecounter;
 
 assign accumulate = framecounter > 0;
 
-spi_shift_mem ss(clk, trig, accumulate, data, cmd, spi_sck, spi_cs, spi_si, spi_so);
+spi_shift_mem ss(clk, trig, accumulate, frame, data, cmd, spi_sck, spi_cs, spi_si, spi_so);
 
 //assign trig = (counter==0)? Q[20:0] == 21'hFFFFFFFF : Q[27:0] == 28'hFFFFFFFF;
-assign trig = Q[25:0] == 26'hFFFFFFFF;
+assign trig = Q[24:0] == 25'hFFFFFFFF;
 
 initial begin
-cmd = 32'b0;
+//cmd = 32'b0;
 Q = 21'b0;
 framecounter = 1'b0;
+//cmd[31:24] <= `CMD_READ;
+
 end
 
-always@(negedge Q[25]) begin
-cmd[31:24] <= `CMD_READ;
-if (counter > 0) begin
-	cmd[18:0] <= (`DATA_SIZE/8) * frames[(framecounter*8)+:8];
+assign cmd[31:24] = `CMD_READ;
+assign cmd[18:0] = (`DATA_SIZE/8) * frames[(frame*8)+:8];
+
+
+//always@(*) cmd[18:0] <= (`DATA_SIZE/8) * frames[(frame*8)+:8];
+
+
+//always@(negedge Q[22]) begin
+//cmd[31:24] <= `CMD_READ;
+//if (counter > 0) begin
+	//cmd[18:0] <= (`DATA_SIZE/8) * frames[(frame*8)+:8];
 	//cmd[18:0] <= (`DATA_SIZE/8) * frames[23:16];
 
-end else begin
-	if (cmd[18:0] >= 19'h4ac00) cmd[18:0] <= 0;
-	else cmd[18:0] <= cmd[18:0] + `DATA_SIZE/8;
-end
+//end else begin
+	//if (cmd[18:0] >= 19'h4ac00) cmd[18:0] <= 0;
+	//else cmd[18:0] <= cmd[18:0] + `DATA_SIZE/8;
+//end
 
-if (framecounter >= 2) framecounter <= 0;
-else framecounter <= framecounter + 1'b1;
+//if (framecounter >= 2) framecounter <= 0;
+//else framecounter <= framecounter + 1'b1;
 
-end
+//end
 
 
 always@(posedge clk) begin
@@ -73,6 +84,7 @@ module spi_shift_mem(
 	input clk,
 	input trig,
 	input accumulate,
+	output reg [3:0] frame,
 	output reg [`DATA_SIZE-1:0] data,
 	input [31:0] cmd,
 	output spi_sck,
@@ -89,6 +101,7 @@ module spi_shift_mem(
 	assign so = spi_so;
 
 	initial begin
+	frame = 4'h0;
 	data = 8'h05;
 	state = 2'b00;
 	counter = 16'h0000;
@@ -99,6 +112,11 @@ module spi_shift_mem(
 	case (state)
 	`STATE_IDLE: begin
 		if (trig == 1) begin
+			spi_cs <= 1'b0;
+			state <= `STATE_COMMAND;
+			counter <= 0;
+			frame <= 0;
+		end else if (0 < frame && frame < `NUM_FRAMES) begin
 			spi_cs <= 1'b0;
 			state <= `STATE_COMMAND;
 			counter <= 0;
@@ -118,6 +136,7 @@ module spi_shift_mem(
 		if (counter >= `DATA_SIZE - 1) begin
 			state <= `STATE_IDLE;
 			spi_cs <= 1'b1;
+			frame <= frame + 1'b1;
 		end
 	end
 	endcase
@@ -125,7 +144,8 @@ module spi_shift_mem(
 
 	always@(posedge clk) begin
 		if (state == `STATE_DATA) begin
-			if (accumulate)
+			//if (accumulate)
+			if (frame > 0)
 				data <= { data[`DATA_SIZE-2:0], (so | data[`DATA_SIZE-1])};
 			else
 				data <= { data[`DATA_SIZE-2:0], so };
@@ -147,14 +167,18 @@ module spi_shift_testbench;
 	reg [31:0] cmd;
 	wire [7:0] leds;
 	reg accumulate;
+	wire [3:0] frame;
+	reg [31:0] frames;
 
-	spi_shift_mem ssm (clk, trig, accumulate, data, cmd, spi_sck, spi_cs, spi_si, spi_so);
+	spi_shift_mem ssm (clk, trig, accumulate, frame, data, cmd, spi_sck, spi_cs, spi_si, spi_so);
 
 	always@(posedge clk) begin
 	//spi_so <= 1'b1; ///~spi_so;
+	cmd[23:0] <= frames[frame*8 +: 8];
 	end
 
 	initial begin
+	frames <= 32'hF00F5511;
 	cmd[31:24] <= `CMD_READ;
 	cmd[23:0] <= 0;
 
@@ -163,11 +187,16 @@ module spi_shift_testbench;
 	trig = 1'b0;
 	trig <= #5 1'b1;
 	trig <= #10 1'b0;
+	spi_so = 1'b1;
 	
-	repeat(256) #1 clk = ~clk;
+	repeat(100) #1 clk = ~clk;
+
+	spi_so = 1'b0;
+
+	repeat(100) #1 clk = ~clk;
 	
-	trig <= #5 1'b1;
-	trig <= #10 1'b0;
+	//trig <= #5 1'b1;
+	//trig <= #10 1'b0;
 	
 	repeat(256) #1 clk = ~clk;
 
